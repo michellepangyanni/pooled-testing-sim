6fba4935abf808052e436e856b66a1a544a51fc6
henrytran360 <henrytran360@gmail.com>
2024-09-05 02:10:11 -0500

    Implemented Binary Search

--- StudentProtocols/team32.py
+++ StudentProtocols/team32.py
@@ -6,27 +6,22 @@ class Protocol:
         self.p = prevalence
     
     def isolate_positives(self, sample: TestSample) -> set:
-        # Implement your strategy here.
-        # 
-        # 
-        # Your goal is to return the indices of the positive samples
-        # while minimizing the number of calls to sample.query
-        #
-        # Not correctly identifying the set of positive samples results in disqualification!
-        #
-        # The input will be a TestSample object, which allows you to query
-        # a subset of 0 ... n-1
-        #
-        # sample.query([1, 3, 4]) will return True if any of 1, 3, or 4 are positive
-        # return {i for i in range(self.n) if sample.query(set([i]))}
+        def binary_search(pool):
+            if len(pool) == 1:
+                return set(pool) if sample.query(pool) else set()
+            else:
+                mid = len(pool) // 2
+                left, right = pool[:mid], pool[mid:]
 
-        if sample.query(set)(range(self.n)):
-            # true
-            return sample.query(set)(range(0, self.n /2))
-            return sample.query(set)(range(self.n / 2 , self.n))
-            
-        else 
-            # false
-            return {}
+                positives = set()
 
-       
+                if sample.query(left):
+                    positives = positives.union(binary_search(left))
+
+                if sample.query(right):
+                    positives = positives.union(binary_search(right))
+
+                return positives
+
+        full_pool = list(range(self.n))
+        return binary_search(full_pool)
